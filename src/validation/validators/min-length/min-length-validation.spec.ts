84533244d3b1ced6b561edd39ca2a506e62ce79d
Jose Pereira <josepsneto@gmail.com>
2021-12-05 16:12:06 -0300

    test: ensures MinLengthValidation returns falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -2,9 +2,15 @@ import { InvalidFieldError } from '@/validation/errors'
 import { MinLengthValidatior } from '@/validation/validators/min-length/min-length-validation'
 
 describe('MinLengthValidation', () => {
-  test('Should return error id value is invalid', () => {
+  test('Should return error if value is invalid', () => {
     const sut = new MinLengthValidatior('field', 5)
     const error = sut.validate('123')
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy value is valid', () => {
+    const sut = new MinLengthValidatior('field', 5)
+    const error = sut.validate('12345')
+    expect(error).toBeFalsy()
   })
 })
