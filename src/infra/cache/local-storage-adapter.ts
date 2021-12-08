514c929f63ad7d3149e768fc739e3d78fd2e985f
Jose Pereira <josepsneto@gmail.com>
2021-12-08 18:10:08 -0300

    feat: change Login coomposition ro add SaveAccessToken

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -1,6 +1,6 @@
 import { SetStorage } from '@/data/protocols/cache/set-storage'
 
-export class LocalSrorageAdapter implements SetStorage {
+export class LocalStorageAdapter implements SetStorage {
   async set (key: string, value: any): Promise<void> {
     localStorage.setItem(key, value)
   }
--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Login } from '@/presentation/pages'
 import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory'
+import { makeLocalSaveAccessToken } from '@/main/factories/usecases/save-access-token/local-save-access-token-factory'
 import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory'
 
 export const makeLogin: React.FC = () => {
   return (
     <Login
       authentication={makeRemoteAuthentication()}
-      validation={makeLoginValidation()} />
+      validation={makeLoginValidation()}
+      saveAccessToken={makeLocalSaveAccessToken()}/>
   )
 }
