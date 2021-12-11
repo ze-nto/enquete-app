6ab8f20340fc057d627559146c166a279dae1e54
Jose Pereira <josepsneto@gmail.com>
2021-12-11 12:34:40 -0300

    refactor: add SubmitButton Component

---
+++ src/presentation/components/submit-button/submit-button.tsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import Context from '@/presentation/context/form/form-context'
+
+type Props = {
+  text: string
+}
+
+const Input: React.FC<Props> = ({ text }) => {
+  const { state } = useContext(Context)
+  return (
+    <button data-testid="submit" disabled={state.isFormInvalid} type="submit">{text}</button>
+  )
+}
+
+export default Input
