4ec64769640d0e486b5c3bbacc0d4466562616e5
Jose Pereira <josepsneto@gmail.com>
2021-12-11 15:04:36 -0300

    test: adds SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,15 @@
+import { makeSignUpValidation } from '@/main/factories/pages/signup/signup-validation-factory'
+import { ValidationComposite } from '@/validation/validators'
+import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+
+describe('LoginValidationFactory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation()
+    expect(composite).toEqual(ValidationComposite.build([
+      ...Builder.field('name').required().min(5).build(),
+      ...Builder.field('email').required().email().build(),
+      ...Builder.field('password').required().min(5).build(),
+      ...Builder.field('passwordConfirmation').required().min(5).sameAs('password').build()
+    ]))
+  })
+})
