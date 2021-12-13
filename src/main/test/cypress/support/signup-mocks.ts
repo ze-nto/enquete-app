7a5c23b3df14937e4b3592845de72e6c4801cb9e
Jose Pereira <josepsneto@gmail.com>
2021-12-13 19:45:44 -0300

    test: ensures SignUp presents Unexpected Error if server fails

--- src/main/test/cypress/support/signup-mocks.ts
+++ src/main/test/cypress/support/signup-mocks.ts
@@ -1,3 +1,4 @@
 import * as Helper from './http-mock'
 
 export const mockEmailInUseError = (): void => Helper.mockEmailInUseError(/signup/)
+export const mockUnexpectedError = (): void => Helper.mockUnexpectedError(/signup/, 'POST')
