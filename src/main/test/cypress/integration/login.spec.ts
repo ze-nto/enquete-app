0eadf1b07fb79f440898bd9cdabb5d899e27eee7
Jose Pereira <josepsneto@gmail.com>
2021-12-13 10:48:08 -0300

    test: ensures Login correct behavior on request and response

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -46,7 +46,7 @@ describe('Login', () => {
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 
-  it('Should presents error if invalid credentials are provided', () => {
+  it('Should presents InvalidCredentialerror on 401', () => {
     cy.intercept('POST', /login/, {
       statusCode: 401,
       body: {
@@ -58,6 +58,36 @@ describe('Login', () => {
     cy.getByTestId('submit').click()
     cy.getByTestId('spinner').should('not.exist')
     cy.getByTestId('main-error').should('contain.text', 'Credenciais inválidas')
+    cy.url().should('equal', `${baseUrl}/login`)
+  })
+
+  it('Should presents UnexpectedError on any other error', () => {
+    cy.intercept('POST', /login/, {
+      statusCode: faker.random.arrayElement([400, 500]),
+      body: {
+        error: faker.random.words()
+      }
+    })
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('spinner').should('not.exist')
+    cy.getByTestId('main-error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve')
+    cy.url().should('equal', `${baseUrl}/login`)
+  })
+
+  it('Should presents UnexpectedError if invalidData is returned', () => {
+    cy.intercept('POST', /login/, {
+      statusCode: 200,
+      body: {
+        invalidProperty: faker.datatype.uuid()
+      }
+    })
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('spinner').should('not.exist')
+    cy.getByTestId('main-error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve')
     cy.url().should('equal', `${baseUrl}/login`)
   })
 
