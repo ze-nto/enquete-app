6b65d9d4be2f6584d9b8b5ff08d0f5158096ff83
Jose Pereira <josepsneto@gmail.com>
2021-12-15 14:11:14 -0300

    feat: add initial SurveyList view

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,4 +1,4 @@
-import { SignUp } from '@/presentation/pages'
+import { SurveyList } from '@/presentation/pages'
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
@@ -13,6 +13,7 @@ const Router: React.FC<Factory> = (factory: Factory) => {
       <Switch>
         <Route path='/login' exact component={factory.makeLogin} />
         <Route path='/signup' exact component={factory.makeSignUp} />
+        <Route path='/' exact component={SurveyList} />
       </Switch>
     </BrowserRouter>
   )
---
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -0,0 +1,38 @@
+import { Footer, Logo } from '@/presentation/components'
+import React from 'react'
+import Styles from './survey-list.scss'
+
+const SurveyList: React.FC = () => {
+  return (
+    <div className={Styles.surveyListWrap}>
+      <header className={Styles.headerWrap}>
+        <div className={Styles.headerContent}>
+          <Logo />
+          <div className={Styles.logoutWrap}>
+            <span>Jose</span>
+            <a href='#'>Sair</a>
+          </div>
+        </div>
+      </header>
+      <div className={Styles.contentWrap}>
+        <h2>Enquetes</h2>
+        <ul>
+          <li>
+            <div className={Styles.surveyContent}>
+              <time>
+                <span className={Styles.day}>14</span>
+                <span className={Styles.month}>dez</span>
+                <span className={Styles.year}>2021</span>
+              </time>
+              <p>Qual é o seu framework favorito?</p>
+            </div>
+            <footer>Ver resultado</footer>
+          </li>
+        </ul>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default SurveyList
