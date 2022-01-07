d27bec8912e545841e18e7f0ef8b7e3e108546de
Jose Pereira <josepsneto@gmail.com>
2022-01-07 16:58:30 -0300

    test: ensures LoacalStorageAdapter calls removeItem if value is null

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -17,6 +17,14 @@ describe('LocalStorageAdapter', () => {
     expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
   })
 
+  test('Should calls localStorage.removeItem if value is null', () => {
+    const sut = makeSut()
+    const key = faker.database.column()
+    const value = faker.random.objectElement<{}>()
+    sut.set(key, undefined)
+    expect(localStorage.removeItem).toHaveBeenCalledWith(key)
+  })
+
   test('Should calls localStorage.getItem with correct value', () => {
     const sut = makeSut()
     const key = faker.database.column()
