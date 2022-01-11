5a297ad4ec8e093005b9fcf99527906d62cd993e
Jose Pereira <josepsneto@gmail.com>
2022-01-11 19:42:33 -0300

    chore: adds webpack prod and dev configs

---
+++ webpack.prod.js
@@ -0,0 +1,50 @@
+const { DefinePlugin } = require('webpack')
+const common = require('./webpack.common.js')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = merge(common, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  plugins: [
+    new DefinePlugin({
+      'process.env.API_URL': JSON.stringify('http://fordevs.herokuapp.com/api')
+    }),
+    new HtmlWebpackPlugin({
+      template: './template.prod.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main-bundle-[fullhash].css'
+    })
+  ]
+})
